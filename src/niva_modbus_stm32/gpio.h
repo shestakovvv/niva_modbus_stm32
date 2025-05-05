3fe307525a20504da6f44a732b6cd74354ec1cb3
shestakovvv <shestakovvvaleriy@gmail.com>
2025-05-05 15:49:18 +0300

    move to registers access

--- src/niva_modbus_stm32/gpio.h
+++ src/niva_modbus_stm32/gpio.h
@@ -1,16 +1,16 @@
 #include "niva_modbus_stm32_config.h"
 
 static inline void modbus_gpio_on(GPIO_TypeDef* port, uint16_t pin) {
-    LL_GPIO_SetOutputPin(port, (uint32_t)pin);
-        // port->BSRR = (uint32_t)pin;
+    // LL_GPIO_SetOutputPin(port, (uint32_t)pin);
+    port->BSRR = (uint32_t)pin;
 }
 
 static inline void modbus_gpio_off(GPIO_TypeDef* port, uint16_t pin) {
-    LL_GPIO_ResetOutputPin(port, (uint32_t)pin);
-        // port->BSRR = ((uint32_t)pin << 16);
+    // LL_GPIO_ResetOutputPin(port, (uint32_t)pin);
+    port->BSRR = ((uint32_t)pin << 16);
 }
 
 static inline void modbus_gpio_toggle(GPIO_TypeDef* port, uint16_t pin) {
-    LL_GPIO_TogglePin(port, (uint32_t)pin);
-        // port->ODR ^= pin;
+    // LL_GPIO_TogglePin(port, (uint32_t)pin);
+    port->ODR ^= pin;
 }
